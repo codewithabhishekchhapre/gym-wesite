0f6404fbcb642d3ef3ad63d85708e0196a0d2e05
abhishek chhapre <86595439+codewithabhishekchhapre@users.noreply.github.com>
2023-04-04 17:36:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// var a=10;
+// var b="abhishek";
+// var c=12.5
+
+//  var d=window.parseInt(prompt("enter a number"));
+//  var e=window.parseInt(prompt("enre a value"));
+//  var str=window.prompt("enter a string");
+
+
+// window.print();
+
+// if(d>e)
+// {
+//      window.console.log("d is greater")
+// }
+// else{
+//      window.console.log("e is greter than d")
+// }
+// console.log(str)
+
+// for(var i=0;i<10;i++)
+// {
+//      console.log(i);
+
+// }
+// var j=5;
+// while(j-->0)
+// {
+//      console.log(j);
+// }
+
+
+// console.log(a,b,c)
+// console.log(d+e);
+
+
+var abhishek=4;
+
+
+var arr=['abhishek','mukul',12,15.45];
+
+for(var a =0;a<arr.length;a++)
+{
+     console.log(arr.join("*"));
+}
+
